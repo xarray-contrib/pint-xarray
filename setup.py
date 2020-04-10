b1ba00e82fd42f7e25a67ff1d470fe455cd7fea7
Keewis <keewis@posteo.de>
2020-04-10 21:25:05 +0200

    move the metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='pint-xarray',
-    version='0.0.1',
-    author='Tom Nicholas',
-    author_email='tomnicholas1@googlemail.com',
-    description='Physical units interface to xarray using Pint',
-    packages=find_packages(),
-    url='https://github.com/TomNicholas/pint-xarray',
-    install_requires=['numpy >= 1.17.1',
-                      'xarray >= 0.15.1',
-                      'pint >= 0.12'],
-)
+if __name__ == "__main__":
+    setup()
