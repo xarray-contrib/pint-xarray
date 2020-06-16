d047f0d428d4d3cd27b92d71b7457b05de6bb6d0
Keewis <keewis@posteo.de>
2020-06-17 01:11:12 +0200

    remove the old and commented out assert_equal_with_units function

--- pintxarray/tests/utils.py
+++ pintxarray/tests/utils.py
@@ -94,37 +94,3 @@ def attach_units(obj, units):
 def assert_units_equal(a, b):
     __tracebackhide__ = True
     assert extract_units(a) == extract_units(b)
-
-
-# def assert_equal_with_units(a, b):
-#     # works like xr.testing.assert_equal, but also explicitly checks units
-#     # so, it is more like assert_identical
-#     __tracebackhide__ = True
-#
-#     if isinstance(a, xr.Dataset) or isinstance(b, xr.Dataset):
-#         a_units = extract_units(a)
-#         b_units = extract_units(b)
-#
-#         a_without_units = strip_units(a)
-#         b_without_units = strip_units(b)
-#
-#         assert a_without_units.equals(b_without_units), formatting.diff_dataset_repr(
-#             a, b, "equals"
-#         )
-#         assert a_units == b_units
-#     else:
-#         a = a if not isinstance(a, (xr.DataArray, xr.Variable)) else a.data
-#         b = b if not isinstance(b, (xr.DataArray, xr.Variable)) else b.data
-#
-#         assert type(a) == type(b) or (
-#             isinstance(a, Quantity) and isinstance(b, Quantity)
-#         )
-#
-#         # workaround until pint implements allclose in __array_function__
-#         if isinstance(a, Quantity) or isinstance(b, Quantity):
-#             assert (
-#                 hasattr(a, "magnitude") and hasattr(b, "magnitude")
-#             ) and np.allclose(a.magnitude, b.magnitude, equal_nan=True)
-#             assert (hasattr(a, "units") and hasattr(b, "units")) and a.units == b.units
-#         else:
-#             assert np.allclose(a, b, equal_nan=True)
