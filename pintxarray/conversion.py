33ddd399481e9f947286cebb2032e885bd00e6da
Keewis <keewis@posteo.de>
2020-06-28 00:38:40 +0200

    reorder the array functions

--- pintxarray/conversion.py
+++ pintxarray/conversion.py
@@ -1,27 +1,6 @@
 import pint
 
 
-def array_convert_units(data, unit):
-    """ convert the units of an array
-
-    This is roughly the same as ``data.to(unit)``.
-    """
-    if unit is None:
-        return data
-
-    if not isinstance(unit, (str, pint.Unit)):
-        raise ValueError(f"cannot use {unit!r} as a unit")
-    elif isinstance(unit, str) and not isinstance(data, pint.Quantity):
-        raise ValueError(f"cannot convert a non-quantity using {unit!r} as unit")
-
-    registry = data._REGISTRY if isinstance(unit, str) else unit._REGISTRY
-
-    if not isinstance(data, pint.Quantity):
-        data = registry.Quantity(data, "dimensionless")
-
-    return data.to(unit)
-
-
 def array_attach_units(data, unit, registry=None):
     """ attach a unit to the data
 
@@ -61,6 +40,40 @@ def array_attach_units(data, unit, registry=None):
         registry = unit._REGISTRY
 
     return registry.Quantity(data, unit)
+
+
+def array_convert_units(data, unit):
+    """ convert the units of an array
+
+    This is roughly the same as ``data.to(unit)``.
+
+    Parameters
+    ----------
+    data : quantity or array-like
+        The data to convert. If it is not a quantity, it is assumed to be
+        dimensionless.
+    unit : str or pint.Unit
+        The unit to convert to. If a string ``data`` has to be a quantity.
+
+    Returns
+    -------
+    result : pint.Quantity
+        The converted data
+    """
+    if unit is None:
+        return data
+
+    if not isinstance(unit, (str, pint.Unit)):
+        raise ValueError(f"cannot use {unit!r} as a unit")
+    elif isinstance(unit, str) and not isinstance(data, pint.Quantity):
+        raise ValueError(f"cannot convert a non-quantity using {unit!r} as unit")
+
+    registry = data._REGISTRY if isinstance(unit, str) else unit._REGISTRY
+
+    if not isinstance(data, pint.Quantity):
+        data = registry.Quantity(data, "dimensionless")
+
+    return data.to(unit)
 
 
 def array_extract_units(data):
