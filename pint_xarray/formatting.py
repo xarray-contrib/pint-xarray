149750c384e6ad915fc6acbb1b2f1eea41efba1e
Keewis <keewis@posteo.de>
2020-08-06 19:18:49 +0200

    use unit abbreviations

--- pint_xarray/formatting.py
+++ pint_xarray/formatting.py
@@ -5,7 +5,7 @@ def inline_repr(quantity, max_width):
     magnitude = quantity.magnitude
     units = quantity.units
 
-    units_repr = str(units)
+    units_repr = f"{units:~P}"
     data_repr = format_array_flat(magnitude, max_width - len(units_repr) - 3)
 
     return f"[{units_repr}] {data_repr}"
