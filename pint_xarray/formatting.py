53a5071d37847ac75b411fbc82df4aeec960db73
Keewis <keewis@posteo.de>
2020-08-20 16:47:11 +0200

    fall back to the repr for non-ndarray magnitudes

--- pint_xarray/formatting.py
+++ pint_xarray/formatting.py
@@ -1,3 +1,4 @@
+import numpy as np
 from xarray.core.formatting import format_array_flat
 
 
@@ -6,6 +7,9 @@ def inline_repr(quantity, max_width):
     units = quantity.units
 
     units_repr = f"{units:~P}"
-    data_repr = format_array_flat(magnitude, max_width - len(units_repr) - 3)
+    if isinstance(magnitude, np.ndarray):
+        data_repr = format_array_flat(magnitude, max_width - len(units_repr) - 3)
+    else:
+        data_repr = repr(magnitude)[: max_width - len(units_repr) - 3]
 
     return f"[{units_repr}] {data_repr}"
