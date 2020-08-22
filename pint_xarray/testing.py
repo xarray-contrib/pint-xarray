52bb6bcf153bcc643d6248bafa58f79f5e4231ae
Keewis <keewis@posteo.de>
2020-08-22 17:48:02 +0200

    add a docstring

--- pint_xarray/testing.py
+++ pint_xarray/testing.py
@@ -2,6 +2,18 @@ from . import conversion
 
 
 def assert_units_equal(a, b):
+    """ assert that the units of two objects are equal
+
+    Raises an :py:exc:`AssertionError` if the units of both objects are not
+    equal. ``units`` attributes and attached unit objects are treated
+    differently.
+
+    Parameters
+    ----------
+    a, b : DataArray or Dataset
+        The objects to compare
+    """
+
     __tracebackhide__ = True
 
     assert conversion.extract_units(a) == conversion.extract_units(b)
