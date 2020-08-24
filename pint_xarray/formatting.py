4674197fdfe39ceeceaafc062028a567161cde3d
Keewis <keewis@posteo.de>
2020-08-25 01:42:47 +0200

    use maybe_truncate instead of slicing

--- pint_xarray/formatting.py
+++ pint_xarray/formatting.py
@@ -171,6 +171,6 @@ def inline_repr(quantity, max_width):
     if isinstance(magnitude, np.ndarray):
         data_repr = format_array_flat(magnitude, max_width - len(units_repr) - 3)
     else:
-        data_repr = repr(magnitude)[: max_width - len(units_repr) - 3]
+        data_repr = maybe_truncate(repr(magnitude), max_width - len(units_repr) - 3)
 
     return f"[{units_repr}] {data_repr}"
