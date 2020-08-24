4347a32a4230f0284dc237ea7ab9ce5ad0ff97e3
keewis <keewis@users.noreply.github.com>
2020-08-24 18:50:40 +0200

    expose testing functions as public API (#24)
    * add a assert_units_equal function
    
    * expose the testing module
    
    * set a fallback version
    
    * document the testing functions
    
    * add a docstring
    
    * Update pint_xarray/testing.py
    
    Co-authored-by: Jon Thielen <github@jont.cc>
    
    * Update pint_xarray/testing.py
    
    Co-authored-by: keewis <keewis@users.noreply.github.com>
    
    Co-authored-by: Jon Thielen <github@jont.cc>

---
+++ pint_xarray/testing.py
@@ -0,0 +1,22 @@
+from . import conversion
+
+
+def assert_units_equal(a, b):
+    """ assert that the units of two xarray objects are equal
+
+    Raises an :py:exc:`AssertionError` if the units of both objects are not
+    equal. ``units`` attributes and attached unit objects are compared
+    separately.
+
+    Parameters
+    ----------
+    a, b : DataArray or Dataset
+        The objects to compare
+    """
+
+    __tracebackhide__ = True
+
+    assert conversion.extract_units(a) == conversion.extract_units(b)
+    assert conversion.extract_unit_attributes(a) == conversion.extract_unit_attributes(
+        b
+    )
