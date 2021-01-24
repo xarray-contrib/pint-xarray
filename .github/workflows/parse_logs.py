e112d2df41f5533f3ef9fa6ecd864566cfb989a7
keewis <keewis@users.noreply.github.com>
2021-01-25 00:58:41 +0100

    add a scheduled CI using github actions (#52)
    * import the nightly ci
    
    * add a note stating the source of the CI
    
    * remove the upstream-dev CI on azure pipelines
    
    * empty commit [test-upstream]
    
    * mention the tag in the contributing guide [skip-ci]

---
+++ .github/workflows/parse_logs.py
@@ -0,0 +1,92 @@
+# type: ignore
+import argparse
+import itertools
+import pathlib
+import textwrap
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filepaths", nargs="+", type=pathlib.Path)
+args = parser.parse_args()
+
+filepaths = sorted(p for p in args.filepaths if p.is_file())
+
+
+def extract_short_test_summary_info(lines):
+    up_to_start_of_section = itertools.dropwhile(
+        lambda l: "=== short test summary info ===" not in l,
+        lines,
+    )
+    up_to_section_content = itertools.islice(up_to_start_of_section, 1, None)
+    section_content = itertools.takewhile(
+        lambda l: l.startswith("FAILED"), up_to_section_content
+    )
+    content = "\n".join(section_content)
+
+    return content
+
+
+def extract_warnings(lines):
+    up_to_start_of_section = itertools.dropwhile(
+        lambda l: "=== warnings summary ===" not in l,
+        lines,
+    )
+    up_to_section_content = itertools.islice(up_to_start_of_section, 1, None)
+    section_content = itertools.takewhile(
+        lambda l: not l.startswith("==="),
+        up_to_section_content,
+    )
+    content = "\n".join(section_content)
+    return content
+
+
+def format_log_message(path):
+    py_version = path.name.split("-")[1]
+    summary = f"Python {py_version} Test Summary Info"
+    with open(path) as f:
+        lines = [line.rstrip() for line in f]
+        data = extract_short_test_summary_info(lines)
+        warnings = extract_warnings(lines)
+
+    message = (
+        textwrap.dedent(
+            """\
+            <details><summary>{summary}</summary>
+
+            ```
+            {data}
+            ```
+
+            </details>
+            """
+        )
+        .rstrip()
+        .format(summary=summary, data=data)
+    )
+
+    if warnings:
+        message += (
+            textwrap.dedent(
+                """
+
+                <details><summary>Warnings</summary>
+
+                ```
+                {warnings}
+                ```
+
+                </details>
+                """
+            )
+            .rstrip()
+            .format(warnings=warnings)
+        )
+
+    return message
+
+
+print("Parsing logs ...")
+message = "\n\n".join(format_log_message(path) for path in filepaths)
+
+output_file = pathlib.Path("pytest-logs.txt")
+print(f"Writing output file to: {output_file.absolute()}")
+output_file.write_text(message)
