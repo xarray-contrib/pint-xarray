c6c3174300521f24d37dca76bf77c440d16a95e3
Keewis <keewis@posteo.de>
2021-02-08 16:37:18 +0100

    remove the duplicated conversion functions

--- pint_xarray/tests/utils.py
+++ pint_xarray/tests/utils.py
@@ -3,9 +3,9 @@ from contextlib import contextmanager
 
 import numpy as np
 import pytest
-import xarray as xr
-from pint.quantity import Quantity
 from xarray.testing import assert_equal, assert_identical  # noqa: F401
+
+from ..conversion import extract_units
 
 
 @contextmanager
@@ -18,79 +18,6 @@ def raises_regex(error, pattern):
         raise AssertionError(
             f"exception {excinfo.value!r} did not match pattern {pattern!r}"
         )
-
-
-def array_extract_units(obj):
-    if isinstance(obj, (xr.Variable, xr.DataArray, xr.Dataset)):
-        obj = obj.data
-
-    try:
-        return obj.units
-    except AttributeError:
-        return None
-
-
-def extract_units(obj):
-    if isinstance(obj, xr.Dataset):
-        vars_units = {
-            name: array_extract_units(value) for name, value in obj.data_vars.items()
-        }
-        coords_units = {
-            name: array_extract_units(value) for name, value in obj.coords.items()
-        }
-
-        units = {**vars_units, **coords_units}
-    elif isinstance(obj, xr.DataArray):
-        vars_units = {obj.name: array_extract_units(obj)}
-        coords_units = {
-            name: array_extract_units(value) for name, value in obj.coords.items()
-        }
-
-        units = {**vars_units, **coords_units}
-    elif isinstance(obj, xr.Variable):
-        vars_units = {None: array_extract_units(obj.data)}
-
-        units = {**vars_units}
-    elif isinstance(obj, Quantity):
-        vars_units = {None: array_extract_units(obj)}
-
-        units = {**vars_units}
-    else:
-        units = {}
-
-    return units
-
-
-def attach_units(obj, units):
-    if isinstance(obj, xr.DataArray):
-        ds = obj._to_temp_dataset()
-        new_name = list(ds.data_vars.keys())[0]
-        units[new_name] = units.get(obj.name)
-        new_ds = attach_units(ds, units)
-        new_obj = obj._from_temp_dataset(new_ds)
-    elif isinstance(obj, xr.Dataset):
-        data_vars = {
-            name: attach_units(array.variable, {None: units.get(name)})
-            for name, array in obj.data_vars.items()
-        }
-
-        coords = {
-            name: attach_units(array.variable, {None: units.get(name)})
-            for name, array in obj.coords.items()
-        }
-
-        new_obj = xr.Dataset(data_vars=data_vars, coords=coords, attrs=obj.attrs)
-    elif isinstance(obj, xr.Variable):
-        new_data = attach_units(obj.data, units)
-        new_obj = obj.copy(data=new_data)
-    elif isinstance(obj, Quantity):
-        raise ValueError(
-            f"cannot attach {units.get(None)} to {obj}: already a quantity"
-        )
-    else:
-        new_obj = Quantity(obj, units.get(None))
-
-    return new_obj
 
 
 def assert_array_units_equal(a, b):
