2ed2c600e460e8ba9c4795252c4f55f9059a5ff0
Keewis <keewis@posteo.de>
2021-03-02 22:02:29 +0100

    add indexer comparison functions

--- pint_xarray/tests/utils.py
+++ pint_xarray/tests/utils.py
@@ -3,9 +3,10 @@ from contextlib import contextmanager
 
 import numpy as np
 import pytest
+from pint import Quantity
 from xarray.testing import assert_equal, assert_identical  # noqa: F401
 
-from ..conversion import extract_units
+from ..conversion import array_strip_units, extract_indexer_units, extract_units
 
 
 @contextmanager
@@ -36,6 +37,41 @@ def assert_array_equal(a, b):
     b_ = getattr(b, "magnitude", b)
 
     np.testing.assert_array_equal(a_, b_)
+
+
+def assert_slice_equal(a, b):
+    attrs = ("start", "stop", "step")
+    values_a = tuple(getattr(a, name) for name in attrs)
+    values_b = tuple(getattr(b, name) for name in attrs)
+    stripped_a = tuple(array_strip_units(v) for v in values_a)
+    stripped_b = tuple(array_strip_units(v) for v in values_b)
+
+    assert (
+        stripped_a == stripped_b
+    ), f"different values: {stripped_a!r} ←→ {stripped_b!r}"
+
+
+def assert_indexer_equal(a, b):
+    __tracebackhide__ = True
+
+    assert type(a) == type(b)
+    if isinstance(a, slice):
+        assert_slice_equal(a, b)
+    elif isinstance(a, (Quantity, np.ndarray)):
+        assert_array_equal(a, b)
+    else:
+        a_ = array_strip_units(a)
+        b_ = array_strip_units(b)
+        assert a_ == b_, f"different values: {a_!r} ←→ {b_!r}"
+
+
+def assert_indexer_units_equal(a, b):
+    __tracebackhide__ = True
+
+    units_a = extract_indexer_units(a)
+    units_b = extract_indexer_units(b)
+
+    assert units_a == units_b, f"different units: {units_a!r} ←→ {units_b!r}"
 
 
 def assert_units_equal(a, b):
