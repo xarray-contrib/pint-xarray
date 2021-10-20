0754f2234aba37feb0d22c5183610918862d9c92
Thomas Nicholas <tegn500@york.ac.uk>
2021-10-20 19:07:52 -0400

    sketch of different tests needed

---
+++ pint_xarray/tests/test_checking.py
@@ -0,0 +1,97 @@
+import pytest
+import pint
+import xarray as xr
+
+from pint import UnitRegistry
+
+from ..checking import expects
+
+ureg = UnitRegistry()
+
+
+class TestExpects:
+    def test_single_arg(self):
+
+        @expects('degC')
+        def above_freezing(temp : pint.Quantity):
+            return temp.magnitude > 0
+
+        f_q = pint.Quantity(20, units='degF')
+        assert above_freezing(f_q) == False
+
+        c_q = pint.Quantity(-2, units='degC')
+        assert above_freezing(c_q) == False
+
+        @expects('degC')
+        def above_freezing(temp : xr.DataArray):
+            return temp.pint.magnitude > 0
+
+        f_da = xr.DataArray(20).pint.quantify(units='degF')
+        assert above_freezing(f_da) == False
+
+        c_da = xr.DataArray(-2).pint.quantify(units='degC')
+        assert above_freezing(c_da) == False
+
+    def test_single_kwarg(self):
+
+        @expects('meters', c='meters / second')
+        def freq(wavelength, c=None):
+            if c is None:
+                c = ureg.speed_of_light
+
+            return c / wavelength
+
+    def test_single_return_value(self):
+
+        @expects('Hz')
+        def period(freq):
+            return 1 / freq
+
+        f = pint.Quantity(10, units='Hz')
+
+        # test conversion
+        T = period(f)
+        assert f.units == 'seconds'
+
+        # test wrong dimensions for conversion
+        ...
+
+    @pytest.mark.xfail
+    def test_multiple_return_values(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_mixed_args_kwargs_return_values(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_invalid_input_types(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_invalid_return_types(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_unquantified_arrays(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_wrong_number_of_args(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_nonexistent_kwarg(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_unexpected_return_value(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_expected_return_value(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_wrong_number_of_return_values(self):
+        raise NotImplementedError
