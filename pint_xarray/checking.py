0b5c7c0811b352f518601fd7f71c8e9f49730eed
Thomas Nicholas <thomas.nicholas@columbia.edu>
2022-01-14 16:17:57 -0500

    correct indentation of docstring

--- pint_xarray/checking.py
+++ pint_xarray/checking.py
@@ -8,70 +8,70 @@ from .accessors import PintDataArrayAccessor  # noqa
 
 def expects(*args_units, return_units=None, **kwargs_units):
     """
-     Decorator which ensures the inputs and outputs of the decorated function are expressed in the expected units.
+    Decorator which ensures the inputs and outputs of the decorated function are expressed in the expected units.
 
-     Arguments to the decorated function are checked for the specified units, converting to those units if necessary, and
-     then stripped of their units before being passed into the undecorated function. Therefore the undecorated function
-     should expect unquantified DataArrays, Datasets, or numpy-like arrays, but with the values expressed in specific units.
+    Arguments to the decorated function are checked for the specified units, converting to those units if necessary, and
+    then stripped of their units before being passed into the undecorated function. Therefore the undecorated function
+    should expect unquantified DataArrays, Datasets, or numpy-like arrays, but with the values expressed in specific units.
 
-     Note that the coordinates of input DataArrays are not checked, only the data.
-     So if your decorated function uses coordinates and you wish to check their units,
-     you should pass the coordinates of interest as separate arguments.
+    Note that the coordinates of input DataArrays are not checked, only the data.
+    So if your decorated function uses coordinates and you wish to check their units,
+    you should pass the coordinates of interest as separate arguments.
 
-     Parameters
-     ----------
-     func : callable
-         Function to decorate. which accepts zero or more xarray.DataArrays or numpy-like arrays as inputs,
-         and may optionally return one or more xarray.DataArrays or numpy-like arrays.
-     *args_units : unit-like or mapping of hashable to unit-like, optional
-         Units to expect for each positional argument given to func.
+    Parameters
+    ----------
+    func : callable
+     Function to decorate. which accepts zero or more xarray.DataArrays or numpy-like arrays as inputs,
+     and may optionally return one or more xarray.DataArrays or numpy-like arrays.
+    *args_units : unit-like or mapping of hashable to unit-like, optional
+     Units to expect for each positional argument given to func.
 
-         The decorator will first check that arguments passed to the decorated function possess these specific units
-         (or will attempt to convert the argument to these units), then will strip the units before passing the magnitude
-         to the wrapped function.
+     The decorator will first check that arguments passed to the decorated function possess these specific units
+     (or will attempt to convert the argument to these units), then will strip the units before passing the magnitude
+     to the wrapped function.
 
-         A value of None indicates not to check that argument for units (suitable for flags and other non-data
-         arguments).
+     A value of None indicates not to check that argument for units (suitable for flags and other non-data
+     arguments).
     return_units : unit-like or mapping of hashable to unit-like or list of unit-like or list of mapping of hashable to unit-like, optional
-         The expected units of the returned value(s), either as a single unit or as a list of units. The decorator
-         will attach these units to the variables returned from the function.
+     The expected units of the returned value(s), either as a single unit or as a list of units. The decorator
+     will attach these units to the variables returned from the function.
 
-         A value of None indicates not to attach any units to that return value (suitable for flags and other
-         non-data results).
-     kwargs_units : mapping of hashable to unit-like, optional
-         Unit to expect for each keyword argument given to func.
+     A value of None indicates not to attach any units to that return value (suitable for flags and other
+     non-data results).
+    kwargs_units : mapping of hashable to unit-like, optional
+     Unit to expect for each keyword argument given to func.
 
-         The decorator will first check that arguments passed to the decorated function possess these specific units
-         (or will attempt to convert the argument to these units), then will strip the units before passing the magnitude
-         to the wrapped function.
+     The decorator will first check that arguments passed to the decorated function possess these specific units
+     (or will attempt to convert the argument to these units), then will strip the units before passing the magnitude
+     to the wrapped function.
 
-         A value of None indicates not to check that argument for units (suitable for flags and other non-data
-         arguments).
+     A value of None indicates not to check that argument for units (suitable for flags and other non-data
+     arguments).
 
-     Returns
-     -------
-     return_values : Any
-         Return values of the wrapped function, either a single value or a tuple of values. These will be given units
-         according to return_units.
+    Returns
+    -------
+    return_values : Any
+     Return values of the wrapped function, either a single value or a tuple of values. These will be given units
+     according to return_units.
 
-     Raises
-     ------
-     TypeError
-         If an argument or return value has a specified unit, but is not an xarray.DataArray or pint.Quantity.
-         Also thrown if any of the units are not a valid type, or if the number of arguments or return values does not
-         match the number of units specified.
+    Raises
+    ------
+    TypeError
+     If an argument or return value has a specified unit, but is not an xarray.DataArray or pint.Quantity.
+     Also thrown if any of the units are not a valid type, or if the number of arguments or return values does not
+     match the number of units specified.
 
-     Examples
-     --------
+    Examples
+    --------
 
-     Decorating a function which takes one quantified input, but returns a non-data value (in this case a boolean).
+    Decorating a function which takes one quantified input, but returns a non-data value (in this case a boolean).
 
-     >>> @expects("deg C")
-     ... def above_freezing(temp):
-     ...     return temp > 0
+    >>> @expects("deg C")
+    ... def above_freezing(temp):
+    ...     return temp > 0
 
 
-     TODO: example where we check units of an optional weighted kwarg
+    TODO: example where we check units of an optional weighted kwarg
     """
 
     # Check types of args_units, kwargs_units, and return_units
