f3904f94ff17f6edd7ce34dec675c4f305bd04f5
Keewis <keewis@posteo.de>
2022-03-22 22:59:38 +0100

    remove checks of reindex that just check xarray behavior

--- pint_xarray/tests/test_accessors.py
+++ pint_xarray/tests/test_accessors.py
@@ -1081,18 +1081,6 @@ def test_chunk(obj):
             id="Dataset-incompatible units",
         ),
         pytest.param(
-            xr.Dataset(
-                {
-                    "a": ("x", [10, 20, 30], {"units": unit_registry.Unit("dm")}),
-                    "b": ("y", [60, 120], {"units": unit_registry.Unit("s")}),
-                }
-            ),
-            {"a": Quantity([1, 3], "s"), "b": Quantity([1], "m")},
-            None,
-            ValueError,
-            id="Dataset-incompatible units-invalid dims",
-        ),
-        pytest.param(
             xr.DataArray(
                 [[0, 1], [2, 3], [4, 5]],
                 dims=("x", "y"),
@@ -1147,20 +1135,6 @@ def test_chunk(obj):
             None,
             ValueError,
             id="DataArray-incompatible units",
-        ),
-        pytest.param(
-            xr.DataArray(
-                [[0, 1], [2, 3], [4, 5]],
-                dims=("x", "y"),
-                coords={
-                    "x": ("x", [10, 20, 30], {"units": unit_registry.Unit("dm")}),
-                    "y": ("y", [60, 120], {"units": unit_registry.Unit("s")}),
-                },
-            ),
-            {"x": Quantity([10, 30], "s"), "y": Quantity([60], "m")},
-            None,
-            ValueError,
-            id="DataArray-incompatible units-invalid dims",
         ),
     ),
 )
