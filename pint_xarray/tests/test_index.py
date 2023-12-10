2c2c8148799e5fe28bdfeab621cb7eb5a9a4cf62
Justus Magin <keewis@posteo.de>
2023-12-10 20:02:18 +0100

    add tests for `create_variables`

--- pint_xarray/tests/test_index.py
+++ pint_xarray/tests/test_index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+import xarray as xr
 from xarray.core.indexes import PandasIndex
 
 from pint_xarray import unit_registry as ureg
@@ -33,3 +34,27 @@ def test_replace():
     assert new.units == old.units
     # no mutation
     assert old.index.equals(old_index)
+
+
+@pytest.mark.parametrize(
+    ["wrapped_index", "units", "expected"],
+    (
+        pytest.param(
+            PandasIndex(pd.Index([1, 2, 3]), dim="x"),
+            {"x": ureg.Unit("m")},
+            {"x": xr.Variable("x", ureg.Quantity([1, 2, 3], "m"))},
+        ),
+        pytest.param(
+            PandasIndex(pd.Index([1j, 2j, 3j]), dim="y"),
+            {"y": ureg.Unit("ms")},
+            {"y": xr.Variable("y", ureg.Quantity([1j, 2j, 3j], "ms"))},
+        ),
+    ),
+)
+def test_create_variables(wrapped_index, units, expected):
+    index = PintIndex(index=wrapped_index, units=units)
+
+    actual = index.create_variables()
+
+    assert list(actual.keys()) == list(expected.keys())
+    assert all([actual[k].equals(expected[k]) for k in expected.keys()])
