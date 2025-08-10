1b53c7c939e2a967ea66052407f0dfc9b7c9ae5f
Justus Magin <keewis@posteo.de>
2025-08-10 19:36:37 +0200

    check that default args work, too

--- pint_xarray/tests/test_expects.py
+++ pint_xarray/tests/test_expects.py
@@ -13,23 +13,32 @@ class TestExpects:
     @pytest.mark.parametrize(
         ["values", "units", "expected"],
         (
-            ((ureg.Quantity(1, "m"), 2), ("mm", None), 500),
-            ((ureg.Quantity(1, "m"), ureg.Quantity(0.5, "s")), ("mm", "ms"), 2),
-            ((xr.DataArray(4).pint.quantify("km"), 2), ("m", None), xr.DataArray(2000)),
+            ((ureg.Quantity(1, "m"), 2), ("mm", None, None), 500),
+            ((ureg.Quantity(1, "m"), ureg.Quantity(0.5, "s")), ("mm", "ms", None), 2),
+            (
+                (xr.DataArray(4).pint.quantify("km"), 2),
+                ("m", None, None),
+                xr.DataArray(2000),
+            ),
             (
                 (
                     xr.DataArray([4, 2, 0]).pint.quantify("cm"),
                     xr.DataArray([4, 2, 1]).pint.quantify("mg"),
                 ),
-                ("m", "g"),
+                ("m", "g", None),
                 xr.DataArray([10, 10, 0]),
+            ),
+            (
+                (ureg.Quantity(16, "m"), 2, ureg.Quantity(4, "s")),
+                ("mm", None, "ms"),
+                2,
             ),
         ),
     )
     def test_args(self, values, units, expected):
         @pint_xarray.expects(*units)
-        def func(a, b):
-            return a / b
+        def func(a, b, c=1):
+            return a / (b * c)
 
         actual = func(*values)
 
