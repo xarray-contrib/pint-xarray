749d458cc50fbf788b5e83d5f504dc74f83ccfa4
Justus Magin <keewis@posteo.de>
2025-08-31 13:54:05 +0200

    rename the exception group

--- pint_xarray/errors.py
+++ pint_xarray/errors.py
@@ -12,37 +12,33 @@ def _add_note(e: Exception, note: str) -> Exception:
     return e
 
 
-def format_error_message(mapping: dict[Hashable, Any], op: str) -> ExceptionGroup:
-    messages = {
-        "attach": "Cannot attach units",
-        "parse": "Cannot parse units",
-        "convert": "Cannot convert variables",
-        "convert_indexers": "Cannot convert indexers",
-    }
-    message = messages.get(op)
-    if message is None:  # pragma: no cover
-        raise ValueError("invalid op")
-
+def construct_exception_group(mapping: dict[Hashable, Any], op: str) -> ExceptionGroup:
     match op:
         case "attach":
+            message = "Cannot attach units"
             errors = [
                 _add_note(e, f"cannot attach units to variable {key!r}: {unit}")
                 for key, (unit, e) in mapping.items()
             ]
         case "parse":
+            message = "Cannot parse units"
             errors = [
                 _add_note(e, f"invalid units for variable {key!r}: {unit} ({type})")
                 for key, (unit, type, e) in mapping.items()
             ]
         case "convert":
+            message = "Cannot convert variables"
             errors = [
                 _add_note(e, f"incompatible units for variable {key!r}")
                 for key, e in mapping.items()
             ]
         case "convert_indexers":
+            message = "Cannot convert indexers"
             errors = [
                 _add_note(e, f"incompatible units for indexer for {key!r}")
                 for key, e in mapping.items()
             ]
+        case _:  # pragma: no cover
+            raise ValueError("invalid op")
 
     return PintExceptionGroup(message, errors)
